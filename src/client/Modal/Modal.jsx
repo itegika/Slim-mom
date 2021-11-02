48a0dd2de6ae1a96609c515d1bef0f95a7002521
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-11-02 11:07:48 +0300

    modal start

--- src/client/Modal/Modal.jsx
+++ src/client/Modal/Modal.jsx
@@ -1,7 +1,56 @@
 import styles from "./Modal.module.scss";
+import React from 'react';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-const Modal = ()=> {
-    return <div></div>
-}
+const ModalWindow = document.querySelector('#modal');
+
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }; 
+  }, []);
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  const handleCloseButtonClick = e => {
+    if (e.target.tagName === 'BUTTON') {
+      console.log(e.target.tagName);
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div
+      className={styles}
+      onClick={handleBackdropClick}
+    >
+      <div className={styles}>
+        <button
+          className={styles}
+          onClick={handleCloseButtonClick}
+        />
+        <div className={styles}>
+          <button
+            className={styles}
+            onClick={handleCloseButtonClick}
+          />
+        </div>
+        <div className={styles}>{children}</div>
+      </div>
+    </div>,
+    document.getElementById('modal'),
+  );
+};
 
 export default Modal;

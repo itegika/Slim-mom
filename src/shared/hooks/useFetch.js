c015a485058fd51386e8ed1ac237f2f95cfde38f
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-11-02 17:21:39 +0200

    add custom hook useFetch

--- src/shared/hooks/useFetch.js
+++ src/shared/hooks/useFetch.js
@@ -1,7 +1,21 @@
-import {useState, usefetch} from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const useFetch = ()=> {
 
-}
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async function() {
+      try {
+        const { data } = await axios.get(url);
+        setData(data);
+      } catch (error) {
+        throw error;
+      }
+    };
+    fetchData();
+  }, [url]);
+  return data;
+};
 
 export default useFetch;

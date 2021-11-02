213eaedf1ee12aa76426b575b64908fbdc89e0a1
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-11-02 15:04:16 +0200

    textInput

--- src/shared/components/TextField/TextField.jsx
+++ src/shared/components/TextField/TextField.jsx
@@ -1,5 +1,21 @@
-const TextField = ()=> {
-    return <input type="text" />
-}
+import {generate} from "shortid";
+
+import styles from "./TextField.module.scss";
+
+const TextField = ({label, ...field})=>{
+    const id = generate();
+
+    return (
+        <div className="form-group">
+            {label && <label htmlFor={id}></label>}
+            <input className = {styles.formInput} 
+                {...field}
+                id={id}
+                />
+        </div>
+    )
+};
+
+
 
 export default TextField;

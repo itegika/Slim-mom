01b35e1ce021146a4731a2a3fb3f5da5ad7133c5
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2021-11-03 11:50:12 +0200

    Component RadioButton and it's styles added

--- src/shared/components/RadioField/RadioField.jsx
+++ src/shared/components/RadioField/RadioField.jsx
@@ -1,3 +1,32 @@
-const RadioField = ()=> {
-    return <input type="radio" />
-}   
+import { nanoid } from "nanoid";
+import styles from "./RadioField.module.scss";
+import PropTypes from "prop-types";
+const RadioField = ({ onChange, required, label, name }) => {
+  const id = nanoid();
+  return (
+    <div className={styles.radioBox}>
+      <input
+        name={name}
+        id={id}
+        type="radio"
+        onChange={onChange}
+        required={required}
+        className={`${styles.radioButton} ${styles.visuallyHidden}`}
+      />
+      <label htmlFor={id} className={styles.radioLabel}>
+        <span className={styles.customRadio}></span>
+        {label && label}
+      </label>
+    </div>
+  );
+};
+RadioField.defaultProps = {
+  onChange: () => {},
+  required: false,
+};
+RadioField.propTypes = {
+  onChange: PropTypes.func,
+  required: PropTypes.bool,
+  label: PropTypes.string,
+};
+export default RadioField;

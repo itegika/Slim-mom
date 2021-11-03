d3c20032333b657c881e478734bb0b8c353299df
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-11-03 11:39:21 +0300

    fixed styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./client/Navbar";
 import Routes from "./navigation/Routes"
 import "./shared/styles/common.sccs";
+import Modal from "./client/Modal/Modal";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
-      <Routes />
+      {/* <Routes /> */}
+      <Modal />
     </div>
   );
 }

ae05ca96bca0ecc67d2d5e293ed795d8e8c1ca7f
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-11-03 11:50:52 +0300

    import modal to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./client/Navbar";
 import Routes from "./navigation/Routes"
 import "./shared/styles/common.sccs";
-
+import Modal from "./client/Modal/Modal";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
       <Routes />
+      <Modal />
     </div>
   );
 }

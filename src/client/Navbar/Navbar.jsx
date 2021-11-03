a6f65348ba8fb7b7e828ea29a87b40ad8586893d
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-11-03 17:21:43 +0200

    add UserMenu and UserInfo, add LogOut button

--- src/client/Navbar/Navbar.jsx
+++ src/client/Navbar/Navbar.jsx
@@ -1,8 +1,41 @@
 // import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../shared/hooks/useAuth";
+import Logo from "../../shared/components/Logo/Logo";
+import UserMenu from "./UserMenu/UserMenu";
+import UserInfo from "./UserInfo/UserInfo";
+import logoMobile from "../../shared/images/logo/logo-mobile.png";
+import logoMobile2x from "../../shared/images/logo/logo-mobile-2x.png";
+import logoTablet from "../../shared/images/logo/logo-tablet.png";
+import logoTablet2x from "../../shared/images/logo/logo-tablet-2x.png";
+import logoDesktop from "../../shared/images/logo/logo-desktop.png";
+import logoDesktop2x from "../../shared/images/logo/logo-desktop-2x.png";
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
-  return <div></div>;
+  const isLoggedIn = useAuth();
+  return (
+    <nav className={styles.nav}>
+      <div className="container">
+        <div className={styles.row}>
+          <div className={styles.logo}>
+            <Link to="/" className={styles.link}>
+              <Logo
+                logoMobile={logoMobile}
+                logoMobile2x={logoMobile2x}
+                logoTablet={logoTablet}
+                logoTablet2x={logoTablet2x}
+                logoDesktop={logoDesktop}
+                logoDesktop2x={logoDesktop2x}
+              />
+            </Link>
+          </div>
+          <UserMenu isLoggedIn={isLoggedIn} />
+          {isLoggedIn && <UserInfo />}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/client/Navbar/UserMenu/UserMenu.jsx
+++ src/client/Navbar/UserMenu/UserMenu.jsx
@@ -1,52 +1,16 @@
-import { Link, NavLink } from "react-router-dom";
-import Logo from "../../shared/components/Logo/Logo";
-import logoMobile from "../../shared/images/logo/logo-mobile.png";
-import logoMobile2x from "../../shared/images/logo/logo-mobile-2x.png";
-import logoTablet from "../../shared/images/logo/logo-tablet.png";
-import logoTablet2x from "../../shared/images/logo/logo-tablet-2x.png";
-import logoDesktop from "../../shared/images/logo/logo-desktop.png";
-import logoDesktop2x from "../../shared/images/logo/logo-desktop-2x.png";
-import styles from "./UserMenu.module.scss";
+import { NavLink } from "react-router-dom";
+import UserMenuItem from "./UserMenuItem";
+import { items } from "./items";
 
-const UserMenu = () => {
-  return (
-    <div>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <div className={styles.logo}>
-            <Link to="/" className={styles.link}>
-              <Logo
-                logoMobile={logoMobile}
-                logoMobile2x={logoMobile2x}
-                logoTablet={logoTablet}
-                logoTablet2x={logoTablet2x}
-                logoDesktop={logoDesktop}
-                logoDesktop2x={logoDesktop2x}
-              />
-            </Link>
-          </div>
-          <div className={styles.navigation}>
-            <NavLink
-              to="/"
-              exact
-              className={styles.navlink}
-              activeClassName={styles.navlinkActive}
-            >
-              Вход
-            </NavLink>
-            <NavLink
-              to="/"
-              exact
-              className={styles.navlink}
-              activeClassName={styles.navlinkActive}
-            >
-              Регистрация
-            </NavLink>
-          </div>
-        </nav>
-      </header>
-    </div>
-  );
+const loginMenu = items.login.map(({ id, ...props }) => (
+  <UserMenuItem key={id} {...props} />
+));
+const logoutMenu = items.lgout.map(({ id, ...props }) => (
+  <UserMenuItem key={id} {...props} />
+));
+
+const UserMenu = ({ isLoggedIn }) => {
+  return <div>{isLoggedIn ? loginMenu : logoutMenu}</div>;
 };
 
 export default UserMenu;

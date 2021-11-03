3ae923c378e0f9e531aac264ec0acc5d4488cb53
Anastasiia-Chikrizova <78729920+Anastasiia-Chikrizova@users.noreply.github.com>
2021-11-03 16:56:56 +0200

    fixed path

--- src/navigation/Routes.jsx
+++ src/navigation/Routes.jsx
@@ -24,7 +24,7 @@ const Routes = () => {
         <PublicRoute
           restricted
           exact
-          path="/registaration"
+          path="/registration"
           redirectTo="/calculator"
         >
           <RegisterPage />
